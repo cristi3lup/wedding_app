7ccea9f478d8ef8e44b49b9b6db927d78bb46d6f
Cristian Lupuleasa <cristi.lupuleasa@ymail.com>
2025-12-08 19:41:45 +0200

    Force upload of static folder

--- wedding_project/settings.py
+++ wedding_project/settings.py
@@ -159,7 +159,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Enable WhiteNoise compression
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Change from 'CompressedManifestStaticFilesStorage' to this:
-  STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
