c30182f7c33707097611eb18675f59842dab9adb
Cristian Lupuleasa <cristi.lupuleasa@ymail.com>
2025-12-26 23:00:59 +0200

    Add missing tailwind dependencies

--- wedding_project/settings.py
+++ wedding_project/settings.py
@@ -205,6 +205,7 @@ else:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Stripe Configuration
+# Stripe Configuration
 STRIPE_PUBLIC_KEY = os.environ.get("STRIPE_PUBLIC_KEY")
 STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY")
 STRIPE_WEBHOOK_SECRET = os.environ.get("STRIPE_WEBHOOK_SECRET")
