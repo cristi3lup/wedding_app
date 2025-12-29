eb83cdcaf7e131235e7c5096263038d73ebf450a
Cristian Lupuleasa <cristi.lupuleasa@ymail.com>
2025-12-29 20:55:34 +0200

    Stripe Redirect 302 fix

--- wedding_project/urls.py
+++ wedding_project/urls.py
@@ -4,13 +4,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib.auth import views as auth_views
-
+from invapp import views as invapp_views
 # URLs that should NOT have a language prefix
 urlpatterns = [
     path('admin/logout/', auth_views.LogoutView.as_view(next_page='/'), name='admin_logout'),
     path('accounts/', include('allauth.urls')),
     path('i18n/', include('django.conf.urls.i18n')),
-
+    path('webhook/stripe/', invapp_views.stripe_webhook, name='stripe_webhook'),
 
     # REMOVED: path('stripe/', include('djstripe.urls'...))
     # We are now using our own custom webhook in invapp.urls
